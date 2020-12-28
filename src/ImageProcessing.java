3cce1534fd78c8242e69fa2ce8d28ac01fbd22f6
dinaragab05 <55923418+dinaragab05@users.noreply.github.com>
2020-12-28 23:44:11 +0200

    Add files via upload

---
+++ src/ImageProcessing.java
@@ -0,0 +1,13 @@
+
+package imageprocessing;
+
+
+public class ImageProcessing {
+
+   
+    public static void main(String[] args) {
+       ChooseImage imageForm=new ChooseImage();
+       imageForm.setVisible(true);
+    }
+    
+}
